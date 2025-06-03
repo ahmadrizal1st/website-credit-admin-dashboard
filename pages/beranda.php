3d7a7794e98be8ac8b07f62dab578ce5b16ac2d2
Michael kevin <kevintompodung80@gmail.com>
2025-06-03 14:26:05 +0700

    feat: beranda.php

---
+++ pages/beranda.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form glass-effect col-9 mx-3 d-flex justify-content-center align-items-center rounded">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12 text-center">
+                    <h3 class="text-white">Selamat Datang di</h3>
+                    <h4 class="text-white mb-4">Dashboard Digitalent</h4>
+                    <div class="mt-3">
+                        <img src="images/logo.png" alt="" width="50px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
